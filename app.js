ba2f7ae4f30e491a6b965b407abaa6cf3db1e81c
Marj-11 <marj.hajyahya@gmail.com>
2020-10-14 22:40:11 +0100

    first commit

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 //  Loader----------------------------------
-document.onreadystatechange = function() {
-    if (document.readyState !== 'complete') {
-        document.querySelector('body').style.visibility = 'hidden';
-        document.querySelector('#loader').style.visibility = 'visible';
-    } else {
-        document.querySelector('#loader').style.display = 'none';
-        document.querySelector('body').style.visibility = 'visible';
-    }
+document.body.setAttribute('class', 'noscroll');
+
+document.getElementById('overlay').style.display = 'block';
+document.getElementById('spinner').style.display = 'block';
+
+window.onload = function() {
+    document.getElementById('spinner').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+
+    document.body.className = document.body.className.replace(/\bnoscroll\b/, '');
 };
 
 //  Time----------------------------------
