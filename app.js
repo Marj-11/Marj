7a54d7d96c5a8a34f05214579f1672aab84e85ed
Marj-11 <marj.hajyahya@gmail.com>
2020-10-14 22:17:30 +0100

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+//  Loader----------------------------------
+document.onreadystatechange = function() {
+    if (document.readyState !== 'complete') {
+        document.querySelector('body').style.visibility = 'hidden';
+        document.querySelector('#loader').style.visibility = 'visible';
+    } else {
+        document.querySelector('#loader').style.display = 'none';
+        document.querySelector('body').style.visibility = 'visible';
+    }
+};
+
 //  Time----------------------------------
 const d = new Date();
 const y = d.getFullYear();
