e99eed0c458a8308c3305e42cc1c7d535702a861
Marj-11 <marj.hajyahya@gmail.com>
2020-10-14 16:03:19 +0100

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
+//  Time----------------------------------
+const d = new Date();
+const y = d.getFullYear();
+document.getElementById('date').innerHTML = y;
+// -----------------------------------------------------------
+
+//  Swiper----------------------------------
+
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
+// -----------------------------------------------------------
+
+// Hamburger----------------------------------
+const burger = document.querySelector('.icon');
+burger.addEventListener('click', function() {
+    burger.classList.toggle('active');
+});
+// -----------------------------------------------------------
+
 let mainNavLinks = document.querySelectorAll('nav ul li a');
 let mainSections = document.querySelectorAll('section');
 
-window.addEventListener('scroll', event => {
+window.addEventListener('scroll', (event) => {
     let fromTop = window.scrollY + 90;
 
-    mainNavLinks.forEach(link => {
+    mainNavLinks.forEach((link) => {
         let section = document.querySelector(link.hash);
 
         if (
@@ -18,7 +51,8 @@ window.addEventListener('scroll', event => {
     });
 });
 
-//////////////////////////////////
+// -----------------------------------------------------------
+
 const nav = document.querySelector('.header');
 let topOfNav = nav.offsetTop;
 
@@ -33,106 +67,13 @@ function fixNav() {
 }
 
 window.addEventListener('scroll', fixNav);
-//////////////////////////////////////////////////////////////////////
-var canvas = document.getElementById('star-canvas');
-var width = document.getElementsByClassName('outer')[0].offsetWidth;
-var height = document.getElementsByClassName('outer')[0].offsetHeight;
-canvas.width = width;
-canvas.height = height;
-var c = canvas.getContext('2d');
-
-function Star(x, y, dx, dy, radius) {
-    this.x = x;
-    this.y = y;
-    this.dx = dx;
-    this.dy = dy;
-    this.radius = radius;
-
-    this.draw = function() {
-        c.beginPath();
-        c.arc(this.x, this.y, this.radius, 0, Math.PI * 22, false);
-        c.closePath();
-        c.fillStyle = 'white';
-        c.fill();
-    };
-
-    this.update = function() {
-        if (this.x + this.radius > width) {
-            this.x = 0;
-        } else if (this.y + this.radius < 0) {
-            this.y = height;
-        }
-
-        this.x += this.dx;
-        this.y += this.dy;
-
-        this.draw();
-    };
-}
-
-function ShootingStar(sx, sy) {
-    this.sx = sx;
-    this.sy = sy;
-    this.sdx = 10;
-    this.sdy = -5;
-    this.radius = 2;
-
-    this.draw = function() {
-        c.beginPath();
-        c.moveTo(this.sx, this.sy);
-        c.lineTo(this.sx + 50, this.sy + 10);
-        c.strokeStyle = 'rgba(255,255,255,0.5)';
-        c.stroke();
-    };
-
-    this.update = function() {
-        if (this.sx + this.radius > width) {} else if (this.sy + this.radius > height) {} else {
-            this.sx += 50;
-            this.sy += 10;
-
-            this.draw();
-        }
-    };
-}
-
-var starsArray = [];
-var shootingStarsArray = [];
-
-for (let i = 0; i < width / 10; i++) {
-    var x = Math.random() * width;
-    var y = Math.random() * height;
-    var dy = -Math.random() / 9;
-    var dx = -dy * 5;
-    var radius = Math.random() * 2;
-
-    starsArray.push(new Star(x, y, dx, dy, radius));
-}
-
-function animate() {
-    requestAnimationFrame(animate);
-    c.clearRect(0, 0, width, height);
-    for (var i = 0; i < starsArray.length; i++) {
-        starsArray[i].update();
-    }
-    for (var i = 0; i < shootingStarsArray.length; i++) {
-        shootingStarsArray[i].update();
-    }
-}
-
-animate();
-
-var int = setInterval(function() {
-    shootingStarsArray = [];
-    var sx = Math.random() * (width / 2);
-    var sy = Math.floor(Math.random() * (height / 2));
-    shootingStarsArray.push(new ShootingStar(sx, sy));
-}, 5000);
+// -----------------------------------------------------------
 
 //------------------------------------------IntersectionObservers----------------------------------------------/
 
 const icons = document.querySelector('#icons');
 
-observer = new IntersectionObserver(entries => {
+observer = new IntersectionObserver((entries) => {
     if (entries[0].intersectionRatio !== 0) {
         entries[0].target.style.animation = `anim1 2s ${entries[0].target.dataset.delay} forwards ease-out`;
     }
@@ -143,7 +84,7 @@ observer.observe(icons);
 
 const skills = document.querySelector('.skills');
 let counter = 0;
-observer = new IntersectionObserver(entries => {
+observer = new IntersectionObserver((entries) => {
     if (entries[0].intersectionRatio !== 0) {
         entries[0].target.style.animation = `skills 1s ${entries[0].target.dataset.delay} forwards ease-out`;
         counter++;
@@ -161,7 +102,7 @@ observer = new IntersectionObserver(entries => {
             if (i == 0) {
                 i = 1;
                 let elem = document.querySelectorAll('#myBar');
-                elem.forEach(e => {
+                elem.forEach((e) => {
                     let width = 1;
                     let idValue = e.innerHTML;
                     let idClass;
@@ -209,7 +150,7 @@ const card1 = document.querySelectorAll('.card1');
 // };
 
 observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
+    entries.forEach((entry) => {
         if (!entry.isIntersecting) {
             return;
         } else {
@@ -219,7 +160,7 @@ observer = new IntersectionObserver((entries, observer) => {
     });
 });
 
-card1.forEach(card => {
+card1.forEach((card) => {
     observer.observe(card);
 });
 
@@ -233,7 +174,7 @@ const contact = document.querySelector('#contact');
 // };
 
 observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
+    entries.forEach((entry) => {
         if (!entry.isIntersecting) {
             return;
         } else {
